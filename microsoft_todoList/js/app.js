865a458bd83a085f5541e812e53bca2a1cf172c2
Sorin Marcu <marcu.sorin.2003@gmail.com>
2022-05-06 03:55:55 +0300

    microsoft todo v2.1

--- microsoft_todoList/js/app.js
+++ microsoft_todoList/js/app.js
@@ -15,7 +15,6 @@ const isWebp = () => {
 isWebp();
 // / checking support of  webp ============================//
 
-// <To do - App> ================================================================================================
 // <interactive> ===================================================================================
 const sidebar = document.querySelector(".sidebar"),
   burger = document.querySelector(".header__burger"),
@@ -53,51 +52,6 @@ searchSubmit.addEventListener("click", () => {
 // </search>
 // </interactive>===================================================================================//
 
-// <functional>===================================================================================
-// list menu variables
-let activeList;
-const listInput = document.querySelector(".tasks-menu__input"),
-  listSubmit = document.querySelector(".tasks-menu__submit"),
-  listBlock = document.querySelector(".tasks-user"),
-  listMainBlock = document.querySelector(".tasks-main");
-
-// tasks menu variables
-const taskMainInput = document.querySelector(".app__interface--text"),
-  taskMainSubmit = document.querySelector(".app__interface--add"),
-  taskMainCheckbox = document.querySelector(".app__interface--checkbox"),
-  taskMainTitle = document.querySelector(".tasks__title-noEdit"),
-  taskMainTitleEditable = document.querySelector(".tasks__title-input");
-
-// tasks active variables
-const tasksBlock = document.querySelector(".app__tasks");
-// arrays
-let lists = [
-  {
-    title: "My day",
-    active: true,
-    main: true,
-    tasks: [],
-  },
-  {
-    title: "Important",
-    active: false,
-    main: true,
-    tasks: [],
-  },
-  {
-    title: "Planned",
-    active: false,
-    main: true,
-    tasks: [],
-  },
-  {
-    title: "Completed",
-    active: false,
-    main: true,
-    tasks: [],
-  },
-];
-
 // <constructor>===================================================================================
 // list constructor
 function List(title, active, main) {
@@ -106,15 +60,8 @@ function List(title, active, main) {
   this.main = main;
   this.tasks = [];
 }
-// task constructor
-function Task(todo, done, important) {
-  this.todo = todo;
-  this.done = done;
-  this.important = important;
-}
 // </constructor> ===================================================================================//
 
-// <functions> ===================================================================================
 // list template function
 const listTemplate = (item, index) => {
   return `<li onclick="updateActive(${index})" class="tasks-group__item _list row ${item.active ? "_active" : ""}">
@@ -132,121 +79,40 @@ const taskTemplate = (item, index) => {
   isImportant: ${item.important}
 </li>`;
 };
-// Update list function
-const updateList = () => {
-  let taskMainTitleBlock = document.querySelector(".tasks");
-  listBlock.innerHTML = "";
-  listMainBlock.innerHTML = "";
-  lists.forEach((item, index) => {
-    if (item.active) activeList = item;
-    if (item.main) {
-      listMainBlock.innerHTML += listTemplate(item, index);
-      if (item.active) {
-        taskMainTitleBlock.classList.add("_main");
-        taskMainTitle.innerHTML = `${item.title}`;
-      }
-    } else {
-      listBlock.innerHTML += listTemplate(item, index);
-      if (item.active) {
-        taskMainTitleBlock.classList.remove("_main");
-        taskMainTitleEditable.value = `${item.title}`;
-        resizeList(taskMainTitleEditable);
-      }
+
+const todo = {
+  variables() {
+    const tasksBlock = document.querySelector(".app__tasks");
+    const taskMainCheckbox = document.querySelector(".app__interface--checkbox");
+    const taskMainInput = document.querySelector(".app__interface--text");
+    let tasks = [];
+    return {
+      tasksBlock: tasksBlock,
+      taskMainCheckbox: taskMainCheckbox,
+      taskMainInput: taskMainInput,
+      tasks: tasks,
+    };
+  },
+  init() {
+    function Task(todo, done, important) {
+      this.todo = todo;
+      this.done = done;
+      this.important = important;
     }
-  });
-  updateTask();
+    todo.variables().taskMainInput.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        todo.createTask(e.target.value, todo.variables().taskMainCheckbox.checked);
+      }
+    });
+  },
+  fillHtml(item, index) {
+    return `<li class="app__tasks--item row">
+    <div>
+      <input onclick="updateDone(${index})" class="app__tasks--checkbox" type="checkbox" ${item.done ? "checked" : ""}/>
+      <span class="app__tasks--title">${item.todo}</span>
+    </div>
+    isImportant: ${item.important}
+  </li>`;
+  },
 };
-// Rename list and resize input functions
-const renameList = (title, isInput = false) => {
-  if (title) {
-    activeList.title = title;
-  } else if (!isInput) {
-    activeList.title = "Untitled list";
-  } else if (isInput) {
-    activeList.title = title;
-  }
-  updateList();
-};
-const resizeList = (item) => {
-  return item.value ? (item.size = item.value.length) : (item.size = 1);
-};
-// update the active element function
-const updateActive = (id = -1) => {
-  lists.forEach((item) => (item.active = false));
-  if (id !== -1) {
-    lists[id].active = true;
-    updateList();
-  }
-};
-// Create list function
-const createList = (title, active = true, main = false) => {
-  updateActive();
-  lists.push(new List(title, active));
-  updateList();
-};
-// Create task function
-const createTask = (todo, done = false, important = false) => {
-  if (activeList.main && activeList.title === "Completed") {
-    activeList.tasks.push(new Task(todo, true, important));
-  } else {
-    activeList.tasks.push(new Task(todo, done, important));
-  }
-  updateList();
-  taskMainInput.value = "";
-  taskMainCheckbox.checked = false;
-  taskMainInput.blur();
-};
-// Update tasks function
-const updateTask = () => {
-  tasksBlock.innerHTML = "";
-  activeList.tasks.forEach((item, index, array) => {
-    tasksBlock.innerHTML += taskTemplate(item, index);
-    if (item.done) {
-      lists[3].tasks.push(item);
-      array.splice(index, 1);
-    } else if (!item.done && activeList === lists[3]) {
-      lists[0].tasks.push(item);
-      array.splice(index, 1);
-    }
-  });
-};
-const updateDone = (index) => {
-  activeList.tasks[index].done = !activeList.tasks[index].done;
-  updateList();
-};
-
-// </functions> ===================================================================================
-
-// <event listening> ===================================================================================
-// Creating new list
-listSubmit.addEventListener("click", () => {
-  createList(listInput.value);
-});
-// Renaming list
-taskMainTitleEditable.addEventListener("input", () => {
-  resizeList(taskMainTitleEditable);
-  renameList(taskMainTitleEditable.value, true);
-});
-taskMainTitleEditable.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    renameList(taskMainTitleEditable.value);
-    taskMainTitleEditable.blur();
-  }
-});
-taskMainTitleEditable.addEventListener("blur", () => {
-  renameList(taskMainTitleEditable.value);
-});
-
-taskMainSubmit.addEventListener("click", () => {
-  createTask(taskMainInput.value, taskMainCheckbox.checked);
-});
-taskMainInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    createTask(taskMainInput.value, taskMainCheckbox.checked);
-  }
-});
-// </event listening> ===================================================================================
-// </functional> ================
-// </To do - App> ================================================================================================
-
-updateList();
+todo.init();
